772412d309a901da12156b5eefd49abbdd1e9c2b
studio-baang <qkddpwns@gmail.com>
2024-11-19 12:13:59 +0900

    딜레이 추가

--- develop/js/pages/contact.js
+++ develop/js/pages/contact.js
@@ -3,25 +3,27 @@ class Contact {
 
     }
     onLoad() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const model = urlParams.get('model'); // 'promo' 파라미터 값 가져오기
+        setTimeout(() => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const model = urlParams.get('model'); // 'promo' 파라미터 값 가져오기
 
-        if (model) {
-            // 파라미터 값의 공백 처리 (디코딩)
-            const decodeModel = decodeURIComponent(model.trim());
+            if (model) {
+                // 파라미터 값의 공백 처리 (디코딩)
+                const decodeModel = decodeURIComponent(model.trim());
 
-            // <select> 요소 선택
-            const selectElement = document.querySelector('select[name="model"]');
+                // <select> 요소 선택
+                const selectElement = document.querySelector('select[name="model"]');
 
-            // <select>에서 해당 값을 가진 옵션 선택
-            for (const option of selectElement.options) {
-                if (option.value === decodeModel) { // 공백이 정상적으로 처리된 값과 비교
-                    option.selected = true; // 해당 옵션 선택
-                    console.log('야발!');
-                    break;
+                // <select>에서 해당 값을 가진 옵션 선택
+                for (const option of selectElement.options) {
+                    if (option.value === decodeModel) { // 공백이 정상적으로 처리된 값과 비교
+                        option.selected = true; // 해당 옵션 선택
+                        console.log('야발!');
+                        break;
+                    }
                 }
             }
-        }
+        }, 500);
     }
 }
 
