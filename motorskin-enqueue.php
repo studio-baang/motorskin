9140ea596394cb42cc8579ba115515d42defcb30
studio-baang <qkddpwns@gmail.com>
2024-11-19 16:52:58 +0900

    잘못 지운 코드 복구

--- motorskin-enqueue.php
+++ motorskin-enqueue.php
@@ -8,13 +8,42 @@ Requires PHP: 7.4
 */
 
 function wpdocs_theme_name_scripts() {
-    wp_enqueue_style( 'motorskin-custom-style', plugin_dir_url( __FILE__ ).'/dist/js/app.css' );
-	wp_enqueue_style( 'motorskin-script-style', plugin_dir_url( __FILE__ ).'/dist/css/custom.css' );
+    wp_enqueue_style( 'motorskin-script-style', plugin_dir_url( __FILE__ ).'/dist/js/app.css' );
+	wp_enqueue_style( 'motorskin-custom-style', plugin_dir_url( __FILE__ ).'/dist/css/custom.css' );
 	wp_enqueue_script( 'motorskin-script', plugin_dir_url( __FILE__ ). '/dist/js/app.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
-
 add_filter('big_image_size_threshold', '__return_false');
+
+
+function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
+    if ( $scanned_tag['name'] != 'model' )  
+        return $scanned_tag;
+
+    $rows = get_posts(
+    	array ( 
+	        'post_type' => 'promotion-1',  
+	        'numberposts' => -1,  
+	        'orderby' => 'date',
+            'order'   => 'DESC', 
+        )
+    );  
+  
+    if ( ! $rows )  
+        return $scanned_tag;
+
+    foreach ( $rows as $row ) {  
+        $scanned_tag['raw_values'][] = $row->post_title . '|' . $row->post_title;
+    }
+
+    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
+
+    $scanned_tag['values'] = $pipes->collect_befores();
+    $scanned_tag['labels'] = $pipes->collect_afters();
+    $scanned_tag['pipes'] = $pipes;
+  
+    return $scanned_tag;  
+}  
 
 add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2);  
 
