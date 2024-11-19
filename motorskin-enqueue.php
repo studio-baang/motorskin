fe5896d244b9ba4dce3cb41330da692fa50c6619
studio-baang <qkddpwns@gmail.com>
2024-11-19 16:04:48 +0900

    css build 안되던 문제 해결

--- motorskin-enqueue.php
+++ motorskin-enqueue.php
@@ -13,42 +13,8 @@ function wpdocs_theme_name_scripts() {
 	wp_enqueue_script( 'motorskin-script', plugin_dir_url( __FILE__ ). '/dist/js/app.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
+
 add_filter('big_image_size_threshold', '__return_false');
-
-// Remove default Oxygen.css
- function lit_dequeue_scripts() {
-	wp_dequeue_style("oxygen");
-}
-add_action( 'oxygen_enqueue_scripts', 'lit_dequeue_scripts' );
-
-function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
-    if ( $scanned_tag['name'] != 'model' )  
-        return $scanned_tag;
-
-    $rows = get_posts(
-    	array ( 
-	        'post_type' => 'promotion-1',  
-	        'numberposts' => -1,  
-	        'orderby' => 'date',
-            'order'   => 'DESC', 
-        )
-    );  
-  
-    if ( ! $rows )  
-        return $scanned_tag;
-
-    foreach ( $rows as $row ) {  
-        $scanned_tag['raw_values'][] = $row->post_title . '|' . $row->post_title;
-    }
-
-    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
-
-    $scanned_tag['values'] = $pipes->collect_befores();
-    $scanned_tag['labels'] = $pipes->collect_afters();
-    $scanned_tag['pipes'] = $pipes;
-  
-    return $scanned_tag;  
-}  
 
 add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2);  
 
@@ -63,6 +29,3 @@ function add_query_arg_to_link_wrapper() {
 
     return add_query_arg( 'model', $model, $contact_page_url );
 }
-
-// dequeue oxygen css
-require_once plugin_dir_path( __FILE__ ).'dequeue-oxygen-css.php';
