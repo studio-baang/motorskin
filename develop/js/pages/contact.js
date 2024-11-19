48ec83c4b66f66eb72a9e09115c8cb2bc1631541
studio-baang <qkddpwns@gmail.com>
2024-11-19 11:55:34 +0900

    app.js, custom.css 빌드

--- develop/js/pages/contact.js
+++ develop/js/pages/contact.js
@@ -4,18 +4,18 @@ class Contact {
     }
     onLoad() {
         const urlParams = new URLSearchParams(window.location.search);
-        const promo = urlParams.get('promo'); // 'promo' 파라미터 값 가져오기
+        const model = urlParams.get('model'); // 'promo' 파라미터 값 가져오기
 
-        if (promo) {
+        if (model) {
             // 파라미터 값의 공백 처리 (디코딩)
-            const decodedPromo = decodeURIComponent(promo.trim());
+            const decodeModel = decodeURIComponent(model.trim());
 
             // <select> 요소 선택
             const selectElement = document.querySelector('select[name="model"]');
 
             // <select>에서 해당 값을 가진 옵션 선택
             for (const option of selectElement.options) {
-                if (option.value === decodedPromo) { // 공백이 정상적으로 처리된 값과 비교
+                if (option.value === decodeModel) { // 공백이 정상적으로 처리된 값과 비교
                     option.selected = true; // 해당 옵션 선택
                     break;
                 }
