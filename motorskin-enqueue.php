236d77964f932177e6f47c9e1f64dd63316281f8
studio-baang <qkddpwns@gmail.com>
2024-11-21 00:14:28 +0900

    메뉴 설명 추가 css 동적 생성 함수 추가

--- motorskin-enqueue.php
+++ motorskin-enqueue.php
@@ -16,6 +16,37 @@ add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 add_filter('big_image_size_threshold', '__return_false');
 
 
+// 메뉴에 설명 추가하기
+function add_dynamic_menu_css() {
+    // 모든 메뉴 위치 가져오기
+    $locations = get_nav_menu_locations();
+    
+    // 'primary'는 테마에 따라 변경 가능
+    if (!isset($locations['gnb'])) {
+        return;
+    }
+
+    // 메뉴 객체와 항목 가져오기
+    $menu = wp_get_nav_menu_object($locations['gnb']);
+    $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+    if (!$menu_items) {
+        return;
+    }
+
+    // CSS 출력 시작
+    echo '<style>';
+    foreach ($menu_items as $menu_item) {
+        if (!empty($menu_item->description)) {
+            // CSS 추가
+            echo "li.menu-item-{$menu_item->ID}::after { content: '" . esc_js($menu_item->description) . "'; }\n";
+        }
+    }
+    echo '</style>';
+}
+add_action('wp_head', 'add_dynamic_menu_css');
+
+// contact 자동 select data 수집
 function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
     if ( $scanned_tag['name'] != 'model' )  
         return $scanned_tag;
@@ -45,7 +76,7 @@ function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {
     return $scanned_tag;  
 }  
 
-add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2);  
+add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2); 
 
 // promotion-1 custom link 생성
 function add_query_arg_to_link_wrapper() {
