3ad799683ae455bc5f9a9c6bfaf0c69d7b4003cd
studio-baang <qkddpwns@gmail.com>
2024-11-20 15:48:07 +0900

    data 문제 수정

--- develop/js/pages/packages.js
+++ develop/js/pages/packages.js
@@ -31,6 +31,7 @@ class Packages {
         const cards = document.querySelectorAll('.package-slider__card');
         cards.forEach(card => {
             console.log(card.dataset.model === data);
+            console.log(typeof card.dataset.model + ":" + card.dataset.model, data)
             card.classList.add('package-slider__card--inactive');
             if (card.dataset.model === data || data === "all") {
                 card.classList.remove('package-slider__card--inactive');
