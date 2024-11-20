68a782123e97a546cd2c70fc3a788b5206926615
studio-baang <qkddpwns@gmail.com>
2024-11-21 01:04:17 +0900

    add toggle anim

--- develop/js/app.js
+++ develop/js/app.js
@@ -1,44 +1,62 @@
+import gsap from "gsap";
 import Contact from "./pages/contact";
 import Packages from "./pages/packages";
 import RemoveDefaultClass from "./utils/removeDefaultClass";
 
 class App {
-    constructor() {
-        this.body = document.querySelector('body');
+	constructor() {
+		this.body = document.querySelector("body");
 
-        this.pageId = {
-            packages: 15,
-            contact: 38
-        }
+		this.pageId = {
+			packages: 15,
+			contact: 38,
+		};
 
-        this.activePage = null;
+		this.activePage = null;
 
-        this.init();
-        this.onLoad();
-    }
+		this.init();
+	}
 
-    containsId(id) {
-        return this.body.classList.contains("page-id-" + id);
-    }
+	containsId(id) {
+		return this.body.classList.contains("page-id-" + id);
+	}
 
-    init() {
-        if (this.containsId(this.pageId.packages)) {
-            this.activePage = new Packages();
-            return false;
-        }
-        if (this.containsId(this.pageId.contact)) {
-            this.activePage = new Contact();
-            return false;
-        }
-    }
+	init() {
+		if (this.containsId(this.pageId.packages)) {
+			this.activePage = new Packages();
+			return false;
+		}
+		if (this.containsId(this.pageId.contact)) {
+			this.activePage = new Contact();
+			return false;
+		}
+		this.onLoad();
+	}
 
-    onLoad() {
-        document.addEventListener("DOMContentLoaded", () => {
-            new RemoveDefaultClass;
-            if (this.activePage.onLoad) {
-                this.activePage.onLoad();
-            }
-        })
-    }
+	onLoad() {
+		document.addEventListener("DOMContentLoaded", () => {
+			new RemoveDefaultClass();
+			if (this.activePage.onLoad) {
+				this.activePage.onLoad();
+			}
+		});
+	}
+
+	toggleGnbAnim() {
+		const items = document.querySelectorAll(".header .menu-item");
+
+		items.forEach((item) => {
+			const animTarget = item.querySelector("a");
+			const animation = gsap.to(animTarget, {
+				paused: true,
+				duration: 0.2,
+				x: 20,
+				ease: "power2.inOut",
+			});
+
+			item.addEventListener("mouseenter", () => animation.play());
+			item.addEventListener("mouseleave", () => animation.reverse());
+		});
+	}
 }
 new App();
