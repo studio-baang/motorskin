2d5e588d9d34a2f39338c1b98103f314be47815b
studio-baang <qkddpwns@gmail.com>
2024-11-21 10:15:56 +0900

    php function 최적화 wpcf7 관련 function을 분리함 auto p tag 생성 기능을 비활성홤

--- motorskin-enqueue.php
+++ motorskin-enqueue.php
@@ -44,37 +44,9 @@ function add_dynamic_menu_css() {
     wp_enqueue_style($handle);
 }
 add_action('wp_enqueue_scripts', 'add_dynamic_menu_css');
-// contact 자동 select data 수집
-function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
-    if ( $scanned_tag['name'] != 'model' )  
-        return $scanned_tag;
 
-    $rows = get_posts(
-    	array ( 
-	        'post_type' => 'promotion-1',  
-	        'numberposts' => -1,  
-	        'orderby' => 'date',
-            'order'   => 'DESC', 
-        )
-    );  
-  
-    if ( ! $rows )  
-        return $scanned_tag;
-
-    foreach ( $rows as $row ) {  
-        $scanned_tag['raw_values'][] = $row->post_title . '|' . $row->post_title;
-    }
-
-    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
-
-    $scanned_tag['values'] = $pipes->collect_befores();
-    $scanned_tag['labels'] = $pipes->collect_afters();
-    $scanned_tag['pipes'] = $pipes;
-  
-    return $scanned_tag;  
-}  
-
-add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2); 
+// contact 7 custom 관련 func 호출
+require_once(plugin_dir_url( __FILE__ ). 'wpcf7.php');
 
 // promotion-1 custom link 생성
 function add_query_arg_to_link_wrapper() {
---
+++ wpcf7.php
@@ -0,0 +1,35 @@
+<?php
+    // contact 자동 select data 수집
+function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
+    if ( $scanned_tag['name'] != 'model' )  
+        return $scanned_tag;
+
+    $rows = get_posts(
+    	array ( 
+	        'post_type' => 'promotion-1',  
+	        'numberposts' => -1,  
+	        'orderby' => 'date',
+            'order'   => 'DESC', 
+        )
+    );  
+  
+    if ( ! $rows )  
+        return $scanned_tag;
+
+    foreach ( $rows as $row ) {  
+        $scanned_tag['raw_values'][] = $row->post_title . '|' . $row->post_title;
+    }
+
+    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
+
+    $scanned_tag['values'] = $pipes->collect_befores();
+    $scanned_tag['labels'] = $pipes->collect_afters();
+    $scanned_tag['pipes'] = $pipes;
+  
+    return $scanned_tag;  
+}  
+
+add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2); 
+
+// remove p tag
+    add_filter('wpcf7_autop_or_not', '__return_false');
