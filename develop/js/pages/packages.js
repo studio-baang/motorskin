70b953fce100ccd30d589f09adef86c94b164ba4
studio-baang <qkddpwns@gmail.com>
2024-11-22 23:02:43 +0900

    package update card의 model 데이터를 여러개 사용해도 문제가 없도록 수정함

--- develop/js/pages/packages.js
+++ develop/js/pages/packages.js
@@ -1,55 +1,57 @@
 import Swiper from "swiper";
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination } from "swiper/modules";
 
-import 'swiper/swiper.min.css';
+import "swiper/swiper.min.css";
 
 class Packages {
-    constructor() {
-        this.packageSlider = new Swiper('.package-slider', {
-            slidesPerView: 'auto',
-            spaceBetween: 30,
-            centeredSlides: true,
-            observeSlideChildren: true,
-            loop: true,
-        });
-        this.packageSlider.slideTo(2);
+	constructor() {
+		this.packageSlider = new Swiper(".package-slider", {
+			slidesPerView: "auto",
+			spaceBetween: 30,
+			observeSlideChildren: true,
+		});
 
-        this.filterWrapper = document.querySelector('.filter-model');
-        this.filterItems = this.filterWrapper.querySelectorAll('.filter-model__item');
-        this.filterActiveItemName = 'filter-model__item--active';
+		this.filterWrapper = document.querySelector(".filter-model");
+		this.filterItems = this.filterWrapper.querySelectorAll(".filter-model__item");
+		this.filterActiveItemName = "filter-model__item--active";
 
-        this.filterWrapper.addEventListener('click', this.onClick.bind(this));
-    }
+		this.filterWrapper.addEventListener("click", this.onClick.bind(this));
+	}
 
-    updateActiveClass(target) {
-        this.filterItems.forEach(item => {
-            item.classList.remove(this.filterActiveItemName);
-        });
-        target.classList.add(this.filterActiveItemName);
-    }
+	updateActiveClass(target) {
+		this.filterItems.forEach((item) => {
+			item.classList.remove(this.filterActiveItemName);
+		});
+		target.classList.add(this.filterActiveItemName);
+	}
 
-    updateSlide(data) {
-        const cards = document.querySelectorAll('.package-slider__card');
-        cards.forEach(card => {
-            card.classList.add('package-slider__card--inactive');
-            if (card.dataset.model === data || data === "all") {
-                card.classList.remove('package-slider__card--inactive');
-            }
-        });
+	updateSlide(data) {
+		const cards = document.querySelectorAll(".package-slider__card");
 
-        this.packageSlider.updateSlides();
-        this.packageSlider.slideTo(2);
-    }
+		cards.forEach((card) => {
+			const modelDataArr = card.dataset.model.split(", ");
 
-    onClick(e) {
-        const target = e.target;
-        if (!target.classList.contains(this.filterActiveItemName) && target.dataset.filterName) {
-            const data = target.dataset.filterName;
+			card.classList.add("package-slider__card--inactive");
+			modelDataArr.forEach((e) => {
+				if (e === data || data === "all") {
+					card.classList.remove("package-slider__card--inactive");
+				}
+			});
+		});
 
-            this.updateSlide(data);
-            this.updateActiveClass(e.target);
-        }
-    }
+		this.packageSlider.updateSlides();
+		this.packageSlider.slideTo(0);
+	}
+
+	onClick(e) {
+		const target = e.target;
+		if (!target.classList.contains(this.filterActiveItemName) && target.dataset.filterName) {
+			const data = target.dataset.filterName;
+
+			this.updateSlide(data);
+			this.updateActiveClass(e.target);
+		}
+	}
 }
 
 export default Packages;
