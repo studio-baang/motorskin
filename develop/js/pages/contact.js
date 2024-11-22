100396d3249b4b17f2fdfaa80af82aa9060af2e8
studio-baang <qkddpwns@gmail.com>
2024-11-22 15:30:24 +0900

    package와 contact 연동되지 않는 문제 발견

--- develop/js/pages/contact.js
+++ develop/js/pages/contact.js
@@ -12,7 +12,7 @@ class Contact {
         setTimeout(() => {
             const urlParams = new URLSearchParams(window.location.search);
             const model = urlParams.get('model'); // 'promo' 파라미터 값 가져오기
-
+            console.log('뭐여 시벌');
             if (model) {
                 // 파라미터 값의 공백 처리 (디코딩)
                 const decodeModel = decodeURIComponent(model.trim());
