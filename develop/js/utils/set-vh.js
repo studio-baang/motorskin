f866588c9579cbf9565c2239b0cce02870999aef
studio-baang <qkddpwns@gmail.com>
2025-01-22 23:32:10 +0900

    fix: vh() 함수 개선 작은 변화는 기록하지 않습니다.

--- develop/js/utils/set-vh.js
+++ develop/js/utils/set-vh.js
@@ -1,4 +1,13 @@
+let lastHeight = window.innerHeight;
+const IGNORE_THRESHOLD = 50; // 50px 이하의 변화는 무시
+
 export function setVh() {
-	let vh = window.innerHeight * 0.01;
+	let newHeight = window.innerHeight;
+
+	// 특정 임계값 이하의 리사이징이면 무시
+	if (Math.abs(newHeight - lastHeight) <= IGNORE_THRESHOLD) return;
+
+	let vh = newHeight * 0.01;
 	document.documentElement.style.setProperty("--vh", `${vh}px`);
+	lastHeight = newHeight; // 높이 갱신
 }
