30be86968c5193eb81cf3b4adc63b67fff3ecbce
studio-baang <qkddpwns@gmail.com>
2025-02-24 18:02:02 +0900

    test: ingnore header 작동 여부 테스트

--- develop/js/utils/resize-ignore-header.js
+++ develop/js/utils/resize-ignore-header.js
@@ -3,6 +3,7 @@ const IGNORE_THRESHOLD = 50; // 50px 이하의 변화는 무시
 
 export function ignoreMobileHeaderWhenResizingWindow(func) {
 	let newHeight = window.innerHeight;
+	console.log("resiing?");
 
 	// 특정 임계값 이하의 리사이징이면 무시
 	if (Math.abs(newHeight - lastHeight) <= IGNORE_THRESHOLD) return;
