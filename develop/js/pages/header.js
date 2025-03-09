7d81bc496be141232ace7542caddbabd9d0f7ed6
studio-baang <qkddpwns@gmail.com>
2025-03-10 03:37:52 +0900

    feat: header toggle class func 추가

--- develop/js/pages/header.js
+++ develop/js/pages/header.js
@@ -1,41 +1,43 @@
-import gsap from "gsap";
+// function closeMenu() {
+// 	const menuContainer = document.querySelector(".oxy-pro-menu-container");
+// 	const menu = document.querySelector(".oxy-pro-menu");
 
-function closeMenu() {
-	const menuContainer = document.querySelector(".oxy-pro-menu-container");
-	const menu = document.querySelector(".oxy-pro-menu");
+// 	menuContainer.addEventListener("click", (e) => {
+// 		const target = e.target;
+// 		if (e.target.classList.contains("oxy-pro-menu-container")) {
+// 			menu.classList.remove("oxy-pro-menu-open");
+// 			document.querySelector(".oxy-pro-menu-container").classList.remove("oxy-pro-menu-open-container");
+// 			const preventOverflows = document.querySelectorAll(".oxy-nav-menu-prevent-overflow");
+// 			for (const item of preventOverflows) {
+// 				item.classList.remove("oxy-nav-menu-prevent-overflow");
+// 			}
 
-	menuContainer.addEventListener("click", (e) => {
-		const target = e.target;
-		if (e.target.classList.contains("oxy-pro-menu-container")) {
-			menu.classList.remove("oxy-pro-menu-open");
-			document.querySelector(".oxy-pro-menu-container").classList.remove("oxy-pro-menu-open-container");
-			const preventOverflows = document.querySelectorAll(".oxy-nav-menu-prevent-overflow");
-			for (const item of preventOverflows) {
-				item.classList.remove("oxy-nav-menu-prevent-overflow");
-			}
+// 			oxygen_pro_menu_unset_static_width(menu);
+// 		}
+// 	});
+// }
 
-			oxygen_pro_menu_unset_static_width(menu);
-		}
+function toggleGnbItemAnim() {
+	const gnbItems = document.querySelectorAll(".gnb__item");
+	const activeClassName = "gnb__item--active";
+	gnbItems.forEach((item) => {
+		item.addEventListener("mouseenter", () => {
+			gnbItems.forEach((el) => el.classList.remove(activeClassName));
+			item.classList.add(activeClassName);
+		});
 	});
 }
 
-function toggleGnbAnim() {
-	const items = document.querySelectorAll(".header .menu-item");
-	items.forEach((item) => {
-		const animTarget = item.querySelector("a");
-		const animation = gsap.to(animTarget, {
-			paused: true,
-			duration: 0.2,
-			x: 20,
-			ease: "power1.inOut",
-		});
-
-		item.addEventListener("mouseenter", () => animation.play());
-		item.addEventListener("mouseleave", () => animation.reverse());
+function toggleGnb() {
+	const gnbEl = document.querySelector(".gnb");
+	const headerIconEl = document.querySelector(".header-icon");
+	headerIconEl.addEventListener("click", (e) => {
+		gnbEl.classList.toggle("gnb--active");
+		headerIconEl.classList.toggle("header-icon--active");
 	});
 }
 
 export function headerContorl() {
-	closeMenu();
-	toggleGnbAnim();
+	toggleGnbItemAnim();
+	toggleGnb();
 }
