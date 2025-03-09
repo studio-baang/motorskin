e2d5f77b72b7920313e3f65a817c900e033a4706
studio-baang <qkddpwns@gmail.com>
2025-03-09 19:58:19 +0900

    feat: 메뉴에 동적 css 추가 구문 제거 gnb에 child-menu를 구현하기 위해 인터렉티브 효과 및 구성을 재구성해야 함 먼저 css로 설명을 임의적으로 추가하던 코드를 수정함

--- motorskin-enqueue.php
+++ motorskin-enqueue.php
@@ -34,36 +34,6 @@ function wpdd_enqueue_css_after_oxygens() {
 	$styles->do_items();
 }
 
-
-// 메뉴에 설명 추가하기
-function add_dynamic_menu_css() {
-    // CSS를 등록할 핸들 (테마나 플러그인 스타일 핸들에 연결 가능)
-    $handle = 'custom-dynamic-style';
-
-    // 빈 스타일을 등록해 핸들 생성 (외부 파일 필요 없음)
-    wp_register_style($handle, false);
-
-    // 메뉴 데이터 가져오기
-    $locations = wp_get_nav_menus();
-    $menu_items = wp_get_nav_menu_items($locations[0]->name);
-    if (!$menu_items) return;
-
-    // 동적 CSS 생성
-    $dynamic_css = '';
-    foreach ($menu_items as $menu_item) {
-        if (!empty($menu_item->description)) {
-            $dynamic_css .= "li.menu-item-{$menu_item->ID}>a::after { content: '" . esc_js($menu_item->description) . "'; }\n";
-        }
-    }
-
-    // 동적 CSS를 핸들에 추가
-    wp_add_inline_style($handle, $dynamic_css);
-
-    // 스타일 출력
-    wp_enqueue_style($handle);
-}
-add_action('wp_enqueue_scripts', 'add_dynamic_menu_css');
-
 // contact 7 custom 관련 func 호출
 require_once(plugin_dir_path( __FILE__ ). '/wpcf7.php');
 
