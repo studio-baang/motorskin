cbb8a51c8ae5aaeac91eb3509f812e59353e7e33
studio-baang <qkddpwns@gmail.com>
2025-05-27 00:53:08 +0900

    feat: 포르쉐 딜러 models 페이지 추가 fix: wpcf7 custom select 부분 코드 최적화

--- wpcf7.php
+++ wpcf7.php
@@ -1,44 +1,45 @@
 <?php
     // contact 자동 select data 수집
-function pine_dynamic_select_field_values ( $scanned_tag, $replace ) {  
-    if ( $scanned_tag['name'] != 'model' )  
-        return $scanned_tag;
+function pine_dynamic_select_field_select ( $scanned_tag, $replace ) {  
 
+    if ($scanned_tag['name'] == 'model' ) {
         // URL에서 'model' 파라미터 값 가져오기
-    $current_model = isset($_GET['model']) ? sanitize_text_field($_GET['model']) : '';
-
-    $rows = get_posts(
-    	array ( 
-	        'post_type' => 'car',  
-	        'numberposts' => -1,  
-	        'orderby' => 'title',
-            'order'   => 'ASC', 
-        )
-    );  
-  
-    if ( ! $rows )  
-        return $scanned_tag;
-
-    foreach ( $rows as $row ) { 
-        $filter_title = preg_replace('/<span[^>]*>(.*?)<\/span>/', '$1', $row->post_title );
-        $scanned_tag['raw_values'][] = $filter_title . '|' . $filter_title;
-    }
-
-    $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
-
-    $scanned_tag['values'] = $pipes->collect_befores();
-    $scanned_tag['labels'] = $pipes->collect_afters();
-    $scanned_tag['pipes'] = $pipes;
-
-    foreach ( $scanned_tag['values'] as $key => $value ) {
-        if ( $value === $current_model ) {
-            $scanned_tag['options'][] = 'default:' . $key + 1; // 해당 인덱스를 기본 선택값으로 설정
+        $current_model = isset($_GET['model']) ? sanitize_text_field($_GET['model']) : '';
+        
+        $rows = get_posts(
+        	array ( 
+	            'post_type' => 'car',  
+	            'numberposts' => -1,  
+	            'orderby' => 'title',
+                'order'   => 'ASC', 
+            )
+        );  
+    
+        if ( ! $rows )  
+            return $scanned_tag;
+    
+        foreach ( $rows as $row ) { 
+            $filter_title = preg_replace('/<span[^>]*>(.*?)<\/span>/', '$1', $row->post_title );
+            $scanned_tag['raw_values'][] = $filter_title . '|' . $filter_title;
         }
+    
+        $pipes = new WPCF7_Pipes($scanned_tag['raw_values']);
+    
+        $scanned_tag['values'] = $pipes->collect_befores();
+        $scanned_tag['labels'] = $pipes->collect_afters();
+        $scanned_tag['pipes'] = $pipes;
+    
+        foreach ( $scanned_tag['values'] as $key => $value ) {
+            if ( $value === $current_model ) {
+                $scanned_tag['options'][] = 'default:' . $key + 1; // 해당 인덱스를 기본 선택값으로 설정
+            }
+        }  
     }
-    return $scanned_tag;  
+    return $scanned_tag;
 }  
 
-add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_values', 10, 2); 
+add_filter( 'wpcf7_form_tag', 'pine_dynamic_select_field_select', 10, 2); 
+
 
 // remove p tag
 add_filter('wpcf7_autop_or_not', '__return_false');
