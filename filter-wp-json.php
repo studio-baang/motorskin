6213a5d4ad925a041352c939c5c7aca79dba8cee
studio-baang <qkddpwns@gmail.com>
2025-06-16 02:10:03 +0900

    test: 새로운 로직 수정

--- filter-wp-json.php
+++ filter-wp-json.php
@@ -1,43 +1,20 @@
 <?php
+function filter_dealer_code_exact_title_match( $args, $request ) {
+    if ( isset( $request['search'] ) && ! empty( $request['search'] ) ) {
+        $search_term = $request['search'];
 
-// dealer code 리스트 접근 차단
-add_filter('rest_post_dispatch', function($result, $server, $request) {
-    $route = $request->get_route();
+        // 's'를 제거하고 'title' 정확 일치로 meta_query 사용
+        unset( $args['s'] );
 
-    // dealer-code GET 요청에 대해서만 필터 적용
-    if (strpos($route, '/wp/v2/dealer-code') === 0 && $request->get_method() === 'GET') {
-        
-        // search 파라미터가 없는 경우 전체 목록 차단
-        $search_term = trim($request->get_param('search'));
-        if (!$search_term) {
-            return new WP_Error('rest_forbidden', '전체 쿠폰 리스트 접근은 허용되지 않습니다.', ['status' => 403]);
-        }
-
-        // // 에러 응답인 경우 그대로 반환
-        // if (is_wp_error($result)) {
-        //     return $result;
-        // }
-
-        // // WP_REST_Response인 경우 결과에서 정확히 일치하는 항목이 있는지 확인
-        // if ($result instanceof WP_REST_Response) {
-        //     $data = $result->get_data();
-        //     $match = false;
-
-        //     foreach ($data as $item) {
-        //         if (isset($item['title']['rendered'])) {
-        //             $item_title = trim($item['title']['rendered']);
-        //             if ($item_title === $search_term) { // 대소문자 정확 일치
-        //                 $match = true;
-        //                 break;
-        //             }
-        //         }
-        //     }
-
-        //     if (!$match) {
-        //         return new WP_Error('no_match', '일치하는 쿠폰이 없습니다.', ['status' => 404]);
-        //     }
-        // }
+        // SQL 쿼리 커스터마이징을 위해 필터를 추가
+        add_filter( 'posts_where', function( $where ) use ( $search_term ) {
+            global $wpdb;
+            // 제목 정확히 일치하는 경우만
+            $where .= $wpdb->prepare( " AND {$wpdb->posts}.post_title = %s", $search_term );
+            return $where;
+        } );
     }
 
-    return $result;
-}, 10, 3);
+    return $args;
+}
+add_filter( 'rest_dealer-code_query', 'filter_dealer_code_exact_title_match', 10, 2 );
