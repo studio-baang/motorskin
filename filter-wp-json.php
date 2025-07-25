ec5f8c93e654424add6c7224ebea50c2819ae1c8
studio-baang <qkddpwns@gmail.com>
2025-07-25 13:23:56 +0900

    delete: 내부 서버 요청 ip 여부 확인 코드 제거

--- filter-wp-json.php
+++ filter-wp-json.php
@@ -37,22 +37,6 @@ function filter_dealer_code_exact_title_match( $args, $request ) {
 }
 add_filter( 'rest_dealer-code_query', 'filter_dealer_code_exact_title_match', 10, 2 );
 
-add_filter( 'rest_pre_dispatch', function ( $result, $server, $request ) {
-    $route = $request->get_route();
-    $method = $request->get_method();
-
-    // 단일 포스트 접근인지 확인 (GET /wp/v2/dealer-code/{id})
-    if ( $method === 'GET' && preg_match( '#^/wp/v2/dealer-code/(\d+)$#', $route, $matches ) ) {
-        return new WP_Error(
-            'rest_forbidden',
-            __( '개별 포스트 접근은 허용되지 않습니다.', 'your-text-domain' ),
-            array( 'status' => 403 )
-        );
-    }
-
-    return $result;
-}, 10, 3 );
-
 
 add_filter('rest_pre_dispatch', function ($response, $server, $request) {
      $route = $request->get_route();
@@ -64,17 +48,14 @@ add_filter('rest_pre_dispatch', function ($response, $server, $request) {
         $headers = getallheaders();
         $internal_flag = isset($headers['X-Internal-Request']) ? $headers['X-Internal-Request'] : '';
 
-        // 조건 1: 내부 IP 또는 로컬호스트
-        $is_internal_ip = in_array($remote_ip, ['127.0.0.1', '::1', '192.168.0.0/16']);
-
-        // 조건 2: 커스텀 헤더를 통한 확인
+        // 조건 1: 커스텀 헤더를 통한 확인
         $is_valid_request = ($internal_flag === 'true');
 
                 // 로그 찍기 (워드프레스 디버그 로그 사용)
         error_log("Incoming IP: " . $remote_ip);
         error_log("Incoming Headers: " . print_r($headers, true));
 
-        if (!$is_internal_ip && !$is_valid_request) {
+        if (!$is_valid_request) {
             return new WP_Error(
                 'rest_forbidden',
                 '이 API는 내부 서버에서만 접근 가능합니다.'.$internal_flag.$is_internal_ip,
