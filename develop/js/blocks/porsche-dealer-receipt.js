b02c1b6d80eb616dbc94698f9e07d8ac37b53e24
studio-baang <qkddpwns@gmail.com>
2025-07-25 23:04:02 +0900

    fix: 구글 폼 연동 에러 메세지 세분화

--- develop/js/blocks/porsche-dealer-receipt.js
+++ develop/js/blocks/porsche-dealer-receipt.js
@@ -78,10 +78,10 @@ export class PorcsheDearerReceipt {
 								if (text === "success") {
 									console.log("문의가 전송되었습니다.");
 								} else {
-									console.error("문의 전송에 실패했습니다.");
+									console.error("문의 전송에 실패했습니다. - 처리 과정 중 에러");
 								}
 							} catch (error) {
-								console.error("문의 전송에 실패했습니다.");
+								console.error("문의 전송에 실패했습니다. - 전송 중 에러");
 							}
 						}
 					},
