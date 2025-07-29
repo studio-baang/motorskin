8ceb17fb62246d9b1b0b3862de65022a2b93e42e
studio-baang <qkddpwns@gmail.com>
2025-07-30 01:21:22 +0900

    fix: 모듈 import 문제 수정

--- develop/js/utils/get-taxonomy-data.js
+++ develop/js/utils/get-taxonomy-data.js
@@ -1,3 +1,5 @@
+import { requestWpJson } from "./wp-json";
+
 export async function getTaxonomyData(addonName) {
 	const originData = await requestWpJson(`/porsche-dealer/wp-json/wp/v2/${addonName}`);
 	const data = originData.map((item) => {
